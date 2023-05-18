1f27e444fef209c0c0e8bd2040b420918d03aad5
Christopher Paredes <topherpl98@gmail.com>
2023-05-18 17:16:19 -0400

    gemas faker, will_paginate y scaffold de tweet

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+55.times do
+    Tweet.create(
+      description: Faker::Lorem.sentence,
+      username: Faker::Internet.username
+    )
+end
